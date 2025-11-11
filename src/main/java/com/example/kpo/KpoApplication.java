17e9508cef012a9307bcd5a286c52c89db7d4a67
Natalia <natalia200218@yandex.ru>
2025-11-11 14:05:42 +0400

    Initial commit

---
+++ src/main/java/com/example/kpo/KpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kpo/KpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
